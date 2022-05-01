b19faf23af777b8f79a0e846dfcc4810f1bd9ce9
Ananad Rao Gaikwad <123agaikwad@gmail.com>
2022-05-01 14:44:14 +0530

    third commit

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://my-user:my_pwd@localhost:27017/mern").then(()=>{
+    console.log("connected");
+}).catch((err)=>{
+    console.log('unable to connect');
+    console.log(err);
+})
 
 app.get('/',(req,res)=>{
     res.send("hellow from anand");
