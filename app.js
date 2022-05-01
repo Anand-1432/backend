c73a6e9055ea2e8a5e6379e5eacf4bc0d0db9365
Ananad Rao Gaikwad <123agaikwad@gmail.com>
2022-05-01 14:53:42 +0530

    fourth commit

--- app.js
+++ app.js
@@ -7,11 +7,23 @@ mongoose.connect("mongodb://my-user:my_pwd@localhost:27017/mern").then(()=>{
 }).catch((err)=>{
     console.log('unable to connect');
     console.log(err);
+});
+
+const userSchema = new mongoose.Schema({
+    name:String,
+    lname:String
 })
+
+const User = mongoose.model('USER',userSchema);
 
 app.get('/',(req,res)=>{
     res.send("hellow from anand");
 });
+
+app.get('/data',async(req,res)=>{
+    const data = await User.find();
+    res.send(data);
+})
 
 app.listen(8000,()=>{
     console.log('listening at port 8000');
