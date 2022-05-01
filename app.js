10ec40357664b8929d92b736b2cc4469d4f7a66b
Ananad Rao Gaikwad <123agaikwad@gmail.com>
2022-05-01 14:38:57 +0530

    second commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send("hellow from backend");
+    res.send("hellow from anand");
 });
 
 app.listen(8000,()=>{
